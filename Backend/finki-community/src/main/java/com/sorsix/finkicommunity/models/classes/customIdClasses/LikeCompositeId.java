b6bd7e4c283c7305d867df5db2ec78d2e33ec6bd
Антонио Аврамовиќ <Antonio.avramovic@gmail.com>
2019-08-06 15:38:38 +0200

    add hashCode and equals to composite classes

--- Backend/finki-community/src/main/java/com/sorsix/finkicommunity/models/classes/customIdClasses/LikeCompositeId.java
+++ Backend/finki-community/src/main/java/com/sorsix/finkicommunity/models/classes/customIdClasses/LikeCompositeId.java
@@ -1,6 +1,7 @@
 package com.sorsix.finkicommunity.models.classes.customIdClasses;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class LikeCompositeId implements Serializable {
     private long postId;
@@ -14,4 +15,17 @@ public class LikeCompositeId implements Serializable {
         this.userId = userId;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LikeCompositeId that = (LikeCompositeId) o;
+        return postId == that.postId &&
+                userId == that.userId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postId, userId);
+    }
 }
--- Backend/finki-community/src/main/java/com/sorsix/finkicommunity/models/classes/customIdClasses/ReplyCompositeId.java
+++ Backend/finki-community/src/main/java/com/sorsix/finkicommunity/models/classes/customIdClasses/ReplyCompositeId.java
@@ -1,6 +1,7 @@
 package com.sorsix.finkicommunity.models.classes.customIdClasses;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ReplyCompositeId implements Serializable {
     private long basePostId;
@@ -12,5 +13,19 @@ public class ReplyCompositeId implements Serializable {
     public ReplyCompositeId(long basePostId, long replyPostId) {
         this.basePostId = basePostId;
         this.replyPostId = replyPostId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReplyCompositeId that = (ReplyCompositeId) o;
+        return basePostId == that.basePostId &&
+                replyPostId == that.replyPostId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(basePostId, replyPostId);
     }
 }
