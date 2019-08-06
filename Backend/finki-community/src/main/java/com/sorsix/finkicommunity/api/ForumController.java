ca3f80e1867e3900003ec9d25448dfa4427818d5
FisnikL <fisnik.limani1@gmail.com>
2019-08-06 14:41:44 +0200

    create angular project

--- Backend/finki-community/src/main/java/com/sorsix/finkicommunity/api/ForumController.java
+++ Backend/finki-community/src/main/java/com/sorsix/finkicommunity/api/ForumController.java
@@ -1,4 +1,21 @@
 package com.sorsix.finkicommunity.api;
 
+import com.sorsix.finkicommunity.models.classes.Post;
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/forum")
 public class ForumController {
+
+
+    @GetMapping("/main")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<Post> getTop10Posts(){
+        return null;
+    }
 }
