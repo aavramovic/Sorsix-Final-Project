f67c7f311a52fe4033d5480d0c404d8bbc17ca91
Антонио Аврамовиќ <Antonio.avramovic@gmail.com>
2019-08-29 15:29:38 +0200

    add username to user response

--- Backend/finki-community-server/src/main/java/com/sorsix/finkicommunity/domain/responses/user/UserResponse.java
+++ Backend/finki-community-server/src/main/java/com/sorsix/finkicommunity/domain/responses/user/UserResponse.java
@@ -9,6 +9,16 @@ public class UserResponse {
     private String errorMessage;
     private boolean valid;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
     public UserResponse() {
     }
 
