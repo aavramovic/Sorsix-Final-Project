e24febc5ff4bdf5e563455f3e8295317ae35a137
FisnikL <fisnik.limani1@gmail.com>
2019-09-01 00:02:01 +0200

    some refactoring

--- Backend/finki-community-server/src/main/java/com/sorsix/finkicommunity/domain/requests/NewPostRequest.java
+++ Backend/finki-community-server/src/main/java/com/sorsix/finkicommunity/domain/requests/NewPostRequest.java
@@ -5,12 +5,12 @@ import javax.validation.constraints.NotNull;
 
 public class NewPostRequest {
     @NotNull @NotEmpty
-    public String content;
-    @NotNull @NotEmpty
     public String title;
-    public Long replyToPostId = null;
+    @NotNull @NotEmpty
+    public String content;
     @NotNull @NotEmpty
     public String courseName;
     @NotNull @NotEmpty
     public String username;
+    public Long replyToPostId = null;
 }
